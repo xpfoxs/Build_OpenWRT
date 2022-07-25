9c6a55d8966ad446c902521441172db3ea473762
xpfoxs <46740443+xpfoxs@users.noreply.github.com>
2022-07-25 12:23:28 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -3,4 +3,5 @@
 echo "Apply custom.sh"
 #git clone https://github.com/vernesong/OpenClash.git -b master                                           package/luci-app-openclash
 cd openwrt
+cp -f user/common/banner package/base-files/files/etc/banner
 sed -i 's/%D %V, %C/OpenWrt By XpFox/g' package/base-files/files/etc/banner
