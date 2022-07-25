30500b17bdeeeaf2d3d8450eeb433ea9f5369e32
xpfoxs <46740443+xpfoxs@users.noreply.github.com>
2022-07-25 12:29:37 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -5,3 +5,4 @@ echo "Apply custom.sh"
 cd openwrt
 cp -f user/common/banner package/base-files/files/etc/banner
 sed -i 's/%D %V, %C/OpenWrt By XpFox/g' package/base-files/files/etc/banner
+cp -f user/common/index_x86.htm package/lean/autocore/files/x86/index.htm
